6f6dfd445f6d5bdb6b5d4747a791dcd4dfdfeb66
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-11-12 19:02:52 +0530

    Fixes #1: test done

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+// testing somthing
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
